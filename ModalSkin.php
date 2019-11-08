018cdc7b7274dd158949f9a3633d2a9c1bcb9fb5
Ricardo Moreno <ricardo.moreno@colimasoft.com>
2019-11-08 17:36:52 -0600

    inicio commit samp

--- ModalSkin.php
+++ ModalSkin.php
@@ -1,7 +1,7 @@
 <div id="ModalSkin" class="modal fade" tabindex="-1" role="dialog">
 				<div class="modal-dialog" role="document">
 					<div class="modal-content">
-						<div class="modal-header" style="background-color:#0389ff;">
+						<div class="modal-header" style="background-color:#f8dfc2;">
 						<img src="img/4zkpandilla.png" width="70" height="70" class="d-inline-block align-top" >
 
 							<p class="modal-title" style="color:white; margin-top: 10px;">¿Desea entrar al clan?</p>
@@ -9,13 +9,13 @@
 
 						</div>
 						<div class="modal-body">
-									<b id="msg_restaurar_sub">Pulse <span class="text-success">Aceptar</span> para ponerte en contacto con el owener</b>
-									<p class="text-danger"><small><span id="msgnotarest">Recuerda <b>saber armas RW & WW [4ZK]</b></span></small></p>
+									<b id="msg_restaurar_sub">Inicie<span class="text-success"> sesión </span> en el servidor y contacte un miembro RiV</b>
+									<p class="text-danger"><small><span id="msgnotarest">Recuerda <b>saber armas RW & WW [RiV]</b></span></small></p>
 						</div>
-						<div class="modal-footer" style="background-color:#ecf0f1;">
-							<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+						<!--<div class="modal-footer" style="background-color:#ecf0f1;">
+							<button type="button" class="btn btn-dark" data-dismiss="modal">Cancelar</button>
 							<button type="button" id="btnDelete" class="btn btn-success">Aceptar</button>
-						</div>
+						</div>-->
 					</div><!-- /.modal-content -->
 				</div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
@@ -25,21 +25,21 @@
 <div id="ModalStats" class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-sm">
     <div class="modal-content">
-	<div class="modal-header" style="background-color:#0389ff;">
+	<div class="modal-header" style="background-color:#f8dfc2;">
 						<img src="img/4zkpandilla.png" width="70" height="70" class="d-inline-block align-top" >
 
 							<p class="modal-title" style="color:white; margin-top: 10px;">Estadística</p>
 							
    
 						</div>
-		<h6>General</h6>
-      <p>Nombre: <mark>4ZonaKillers</mark></p>
-	  <p>Tag:<mark>[4zK]</mark></p>
-	  <p>ID: 1229</p>
-	  <p>Líder Owner: <mark>4zK.[R]okealer.cMx</mark></p>
-	  <p>Integrantes: <mark>43</mark></p>
-	  <p>Zonas: <mark>20</mark>, Aesinatos:<mark>13737</mark></p>
-	  <p>ClanWar: Ganados:<mark>18</mark>, Perdidos:<mark>17</mark></p>
+	<h6>General</h6>
+      <p>Nombre: <mark>Rivals</mark></p>
+	  <p>Tag:<mark>[RiV]</mark></p>
+	  <p>ID: -</p>
+	  <p>Líder Owner: <mark>RiV.pink</mark></p>
+	  <p>Integrantes: <mark>-</mark></p>
+	  <p>Zonas: <mark>20</mark>, Aesinatos:<mark>-</mark></p>
+	  <p>ClanWar: Ganados:<mark>-</mark>, Perdidos:<mark>-</mark></p>
     </div>
   </div>
 </div
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-
 <head>
-  <title>Clan Gang 4zK | SuperDanova</title>
+  <title>Clan Gang RiV | Adrenalina Full</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -10,9 +9,9 @@
   <link rel="stylesheet" href="css/master.css">
   <link rel="stylesheet" href="css/nav.css">
   <link href="css/bootstrap.min.css" rel="stylesheet">
-  <!--<iframe id="audio" src="soundtrack.mp3" frameborder="0"  style="display:none"></iframe>-->
-
-
+  <link rel="icon" type="image/png" href="img/riv.jpg" />
+  <iframe id="audio" src="soundtrack.mp3" frameborder="0"  style="display:none"></iframe>
+  <meta name="theme-color" content="#193725"/>
 </head>
 <body>
 <div class="back"></div>
@@ -25,9 +24,8 @@
 
 <!-- Unsplash copyright -->
 <a class="thx" ></a>
-     
      <div class="bottom">
-          <p>4zk - cMX - Bc</p>
+          <p>[RiV] - Rivals</p>
           <img height="60" width="60" src="https://vignette.wikia.nocookie.net/gta-myths/images/d/d0/TEC9-GTASA-icon.png/revision/latest?cb=20180408160648">
           <img height="60" width="60" src="https://vignette.wikia.nocookie.net/gtawiki/images/5/57/Sawn-offShotgun-GTASA-icon.png/revision/latest?cb=20150609171724">
           <img height="60" width="60" src="https://vignette.wikia.nocookie.net/gta-myths/images/5/58/PumpSG-GTASA-Icon.png/revision/latest?cb=20161226114755"><br>
@@ -36,49 +34,33 @@
           <img height="60" width="60" src="https://forum.gta.world/en/uploads/monthly_2018_05/DesertEagle-GTASA-icon.png.266f524a35864cd0d0fb0c5f7fc45010.png">
      </div>
      <div class="bottom2">
-       
-          <img src="img/disccord.png" title="www.discord.com/4ZonaKillers">
-          <img src="img/facebook.png" title="www.facebook.com/luciano.benitez.56863221">
-         
+          <img src="img/disccord.png" title="www.discord.GG/NeZsMZ">
+          <img src="img/facebook.png" title="www.facebook.com/">    
      </div>
 
      <section class="center slider">
           <div>
                
-               <img class="img-principal" src="img/skin/sk4.png" alt="" title="4zK[eLPIZZERO_P$ycho">
-               <h3>4zK[eLPIZZERO_P$ycho</h3>
-               <p>Líder Warrior</p>
-               <i>"Asesino Ositos pendejos"</i>
+               <img class="img-principal img-fluid" src="img/skin/Skin_293.png" alt="" title="RiV[eLPIZZERO_P$ycho">
+               <h3>eLPIZZERO_P$ycho[RiV</h3>
+               <p>Warrior Killer</p>
+               <i>"Asesino como Michael Myers"</i>
                <img src="img/banderas/mexico.png" alt="">
 
           </div>
           <div>
-               <img class="img-principal" src="img/skin/sk17.png" alt="" title="4zK.Blue">
-               <h3>4zK.Blue</h3>
+               <img class="img-principal img-fluid" src="img/skin/Skin_195.png" alt="" title="RiV.pink">
+               <h3>RiV.Pink</h3>
                <p>Líder Warrior</p>
-               <i>"Papá de los <mark>[NtX]"</mark></i>
+               <i>"Guerrera con causa<span style="color:red;">❤</span></i>
                <img src="img/banderas/mexico.png" alt="">
           </div>
           <div>
-               <img class="img-principal" src="img/skin/sk28.png" alt="" title="4z.Gochez">
-               <h3>4z.Rokealer.cMx</h3>
-               <p>Owner Warrior</p>
-               <i>"Maneja Rw,WW y Sniper y Padre de Tw,NTX alv"</i>
+               <img class="img-principal img-fluid" src="img/skin/Skin_2.png" alt="" title="RiV.Cyclixx">
+               <h3>[RiV]Cyclixx</h3>
+               <p>Warrior Killer</p>
+               <i>"Donde pongo el ojo pongo la bala"</i>
                <img src="img/banderas/argentina.png" alt="">
-          </div>
-          <div>
-               <img class="img-principal" src="img/skin/sk16.png" alt="" title="4z.Gochez">
-               <h3>4z.Gochez</h3>
-               <p>Gang Warrior</p>
-               <i>"MÚSICO Y GAMER SNIPER PARA SIEMPRE"</i>
-               <img src="img/banderas/el-salvador.png" alt="">
-          </div>
-          <div>
-               <img class="img-principal" src="img/skin/sk7.png" alt="" title="4zk.Benito_Camela">
-               <h3>4zk.Benito_Camela</h3>
-               <p>Gang Warrior</p>
-               <i>Bueno en Sniperkill, blanco con una poronga de 30 cm</i>
-               <img src="img/banderas/el-salvador.png" alt="">
           </div>
      </section>
      <script src="js/jquery-2.2.0.min.js" type="text/javascript"></script>
